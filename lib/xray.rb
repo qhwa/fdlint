fc05d9e1cb4b34254c287e76d0ee43da218c8dba
qhwa <qhwa@163.com>
2011-05-10 20:08:49 +0800

    add xray abstract class

---
+++ lib/xray.rb
@@ -0,0 +1,15 @@
+class XRay
+
+    def check_css( text, param={} )
+        true
+    end
+
+    def check_js( text, param={} )
+        true
+    end
+
+    def check_html( text, param={} )
+        true
+    end
+
+end
