f3b8ad0c2761ff68499eadaa8c95fcb6d9de35e7
bencode <bencode@163.com>
2011-07-07 13:37:57 +0800

    修改测试集

--- test/all-tests.rb
+++ test/all-tests.rb
@@ -1,15 +1,22 @@
 require 'test/unit'
 gem 'test-unit'
 require 'test/unit/testsuite'
+
+require_relative 'parser_visitable_test'
+require_relative 'position_info_test'
 require_relative 'css/parser_test'
+require_relative 'css/rule/check_list_rule_test'
 
 module XRayTest
     
     class ALL < Test::Unit::TestSuite
-        
-        def suite
+
+        def self.suite
             tests = []
+            tests << PositionInfoTest.new
+            tests << ParserVisitableTest.new
             tests << CSS::ParserTest.new
+            tests << CSS::Rule::CheckListRuleTest.new
             tests
         end
     end
