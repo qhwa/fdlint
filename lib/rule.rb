4712a2f48b9493a222d3568317a7a48d84addc85
qhwa <qhwa@163.com>
2012-01-14 18:17:40 +0800

    add basic dsl

--- lib/rule.rb
+++ lib/rule.rb
@@ -8,60 +8,151 @@ module XRay
 
   module RuleHelper
 
-
-    def method_missing(name, *arguments, &block)
-      if XRay::Rule.keywords.any? { |kw| name.to_s.start_with? kw.to_s }
-        XRay::Rule.add( name, syntax, &block )
-      else
-        super
-      end
-    end
-
-    def syntax(*type)
-      if type.empty?
-        @syntax
-      else
-        @syntax = type.first
-      end
-    end
-
-    STYLE_TYPES.each do |type|
-      eval <<-RUBY
-      def #{type}
-        syntax #{type.inspect}
-        yield if block_given?
-      end
-      RUBY
+    def method_missing(name, *args, &block)
+      Rule.send(name, *args, &block)
     end
 
   end
 
   module Rule
 
-    @keywords = [
-      :check_file,
-      :check_css_file,
-      :check_js_file,
-      :check_html_file
+    KEYWORDS = [
+      :file
     ]
-
-    @rules = []
 
     class << self
 
-      attr_reader :keywords, :rules
+      @@common_rules = []
 
-      def add( name, syntax = nil, &block )
-        rules << { :syntax => syntax, :name => name , :block => block }
+      def syntax(*type)
+        if type.empty?
+          @syntax || 'common'
+        else
+          @syntax = type.first
+        end
       end
 
-      STYLE_TYPES.each do |syntax|
+      def common(&block)
+        syntax nil
+        yield if block_given?
+      end
+
+      KEYWORDS.each do |kw|
         eval <<-RUBY
-          def #{syntax}_rules
-            rules.select { |r| r[:syntax].nil? or r[:syntax].to_s == "#{syntax}" }
+        def #{kw}_rules
+          @@common_rules.select {|r| r[:name].to_s.include? "#{kw}" }
+        end
+
+        def check_#{kw}( *target, &block )
+          if block_given?
+            self.send :"add_\#{syntax}_rule", :check_#{kw}, &block
+          elsif target.size > 0
+            do_check *target, #{kw}_rules
+          end
+        end
+
+        RUBY
+
+        STYLE_TYPES.each do |syn|
+          eval <<-RUBY
+          def #{syn}_#{kw}_rules
+            #{syn}_rules.select {|r| r[:name].to_s.include? "#{kw}" }
+          end
+
+          def only_#{syn}_#{kw}_rules
+            only_#{syn}_rules.select {|r| r[:name].to_s.include? "#{kw}" }
+          end
+
+          def check_#{syn}_#{kw}( *target, &block )
+            if block_given?
+              self.send :"add_#{syn}_rule", :check_#{syn}_#{kw}, &block
+            elsif target.size > 0
+              do_check *target, #{syn}_#{kw}_rules
+            end
+          end
+
+          RUBY
+        end
+      end
+
+      STYLE_TYPES.each do |syn|
+
+        eval <<-RUBY
+
+          @@#{syn}_rules = []
+
+          def #{syn}
+            syntax #{syn.inspect}
+            yield if block_given?
+          end
+
+          def add_#{syn}_rule( name, &block )
+            @@#{syn}_rules << { :name => name, :block => block }
+          end
+
+          def #{syn}_rules
+            @@common_rules + @@#{syn}_rules
+          end
+
+          def only_#{syn}_rules
+            @@#{syn}_rules
+          end
+
+          def clear_#{syn}_rules
+            @@#{syn}_rules.clear
           end
         RUBY
       end
+
+      def clear_all_rules
+        @@common_rules.clear
+        STYLE_TYPES.each { |syn| send(:"clear_#{syn}_rules") }
+      end
+
+      def do_check( *args , rules )
+        rules.inject([]) do |results, r|
+          result = r[:block].call(*args)
+          results << result if result
+        end
+      end
+
+      def method_missing( name , *args, &block )
+        if cmd = cmd_name(name) and self.respond_to? cmd 
+          send(cmd, name, &block )
+          if block_given?
+            define_singleton_method name do |*tar, &b|
+              if tar.size > 0
+                block.call(*tar)
+              else
+                send(cmd, name, &b )
+                puts "add check rule, #{name}, #{tar}"
+              end
+            end
+          end
+        else
+          super
+        end
+      end
+
+      def cmd_name(name)
+        name = name.to_s
+        KEYWORDS.each do |kw| 
+          if name.start_with? "check_#{kw}" 
+            return :"check_#{kw}"
+          else 
+            STYLE_TYPES.each do |s| 
+              return :"check_#{s}_#{kw}" if name.start_with? "check_#{s}_#{kw}"
+            end
+          end
+        end
+        nil
+      end
+
+      def add_common_rule( name, &block )
+        @@common_rules << { :name => name , :block => block }
+      end
+
+      alias :add_rule :add_common_rule
 
       def import( name )
         load_relative
@@ -73,21 +164,33 @@ module XRay
 
 end
 
-include XRay::RuleHelper
+#include XRay::RuleHelper
+#
 
 
-## Rule DSL example
+if __FILE__ == $0
 
-css {
-  check_file do |file|
-    ['should not be min file'] if file =~ /-min\.css$/
-  end
-}
+  extend XRay::RuleHelper
 
-check_file { |file| 
-  ['should use "-" instead of "_"', :warn] if file =~ /_/
-}
+  ## Rule DSL example
+  css {
+    check_file_without_min do |file|
+      ['should not be min file', :error] if file =~ /-min\.css$/
+    end
 
+  }
 
-## Test
-puts XRay::Rule.css_rules
+  common {
+    check_file { |file| 
+      ['should use "-" instead of "_"', :warn] if file =~ /_/
+    }
+  }
+
+  ## Test
+  puts file_rules
+  puts css_file_rules
+
+  puts check_file_without_min('test-min.css').inspect
+  puts check_css_file('test_a-min.css').inspect
+
+end
