e67a1c3a0fe5a301799b67a2bc16d5386be37b3b
qhwa <qhwa@163.com>
2012-01-20 10:29:14 +0800

    code refactor

--- lib/js/rule/all.rb
+++ lib/js/rule/all.rb
@@ -6,18 +6,10 @@ module XRay
 
       class All < SimpleDelegator
 
-        Dir.glob File.join(File.expand_path(File.dirname(__FILE__)), '*.rb') do |file|
-          if file != File.expand_path(__FILE__)
-            require file
-          end
-        end
-
-        RULES = XRay::JS::Rule.constants.select { |c| c.to_s.end_with? 'Rule' }
-
         attr_reader :rules
 
         def initialize(options = {})
-          @rules = RULES.collect do |name|
+          @rules = load_sub_rules.collect do |name|
             klass = Rule.const_get name
             klass.method(:initialize).arity >= 1 ? klass.new(options) :
                 klass.new
@@ -26,6 +18,12 @@ module XRay
           super @rules
         end
 
+        def load_sub_rules
+          Dir.glob File.expand_path( '*.rb', File.dirname(__FILE__) ) do |file|
+            require file
+          end
+          XRay::JS::Rule.constants.select { |c| c.to_s.end_with? 'Rule' }
+        end
 
 
       end
