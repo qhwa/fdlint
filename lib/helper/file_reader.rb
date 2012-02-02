3a30d7aeb006f402dcb0447fad4ec0049e869b5a
qhwa <qhwa@163.com>
2012-02-02 21:45:57 +0800

    Revert "中文编码转换的代码重构"
    This reverts commit 5a8c37c9e13c5537e93cc734aed67c2d64cb2d04.

--- lib/helper/file_reader.rb
+++ lib/helper/file_reader.rb
@@ -1,5 +1,3 @@
-require_relative 'strenc'
-
 module XRay
   module Helper
     module FileReader
@@ -12,7 +10,7 @@ module XRay
         if File.readable?(path)
           bin = File.read(path)
           text = bin.utf8! || bin
-          [text, text.former_enc ||'ascii-8bit' ]
+          [text, $enc||'ascii-8bit']
         else
           raise ArgumentError.new("File is not readable!")
         end
@@ -20,4 +18,30 @@ module XRay
 
     end
   end
+end
+
+
+class String
+
+  def utf8!
+    %w(ascii-8bit utf-8 ucs-bom shift-jis gb18030 gbk gb2312 cp936).any? do |c|
+      begin
+        if self.respond_to? :encode
+          self.encode!('utf-8', c).force_encoding('utf-8')
+        else
+          require 'iconv'
+          text = Iconv.new('UTF-8', c).iconv(self)
+        end
+        if self =~ /./
+          $enc = c
+          break
+        end
+      rescue
+      end
+    end
+
+    self
+    
+  end
+
 end
