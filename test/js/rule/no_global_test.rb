adb0993a1e6f2bfd7fa33b3dfba4246fb1f1d323
qhwa <qhwa@163.com>
2012-02-07 17:49:08 +0800

    add unit test

--- test/js/rule/no_global_test.rb
+++ test/js/rule/no_global_test.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require_relative 'base_test'
 
 require 'js/rule/no_global'
@@ -39,6 +40,41 @@ module XRayTest
           assert_equal 'hello', ret[1].node.text
           assert_equal '(var,[(var=,b,)],)', ret[2].node.text
           assert_equal '(=,z,200)', ret[3].node.text
+        end
+
+        def test_scope_in_other_function
+          js = <<-JS
+          /**
+           * scope test fixture
+           */
+          (function($){
+              function test(){
+                  var temp;
+              }
+
+              var _createSeperator = function(){
+                   var _self = this,
+                      menuEl = _self.get('menuEl');
+                   temp = [a,b,c];
+                   return new Node(temp).appendTo(menuEl);
+
+               test = 123;
+
+              //var temp2;
+              temp2 = 234;
+                };
+
+          })(jQuery);
+          JS
+
+          ret = parse js 
+          assert_equal [
+            "禁止使用未定义的变量(或全局变量)",
+            "禁止使用未定义的变量(或全局变量)"
+          ], ret.map(&:message)
+          assert_equal '(=,temp,[a,b,c])', ret[0].node.text
+          assert_equal '(=,temp2,234)', ret[1].node.text
+          
         end
         
         def parse(js)
